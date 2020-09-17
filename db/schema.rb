8b2df63df18928d1abcbec700b32ec23f3d6ea4a
fozia@iuj.ac.jp <fozia@iuj.ac.jp>
2020-09-17 07:35:16 +0000

    creating image upload function

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_17_065505) do
+ActiveRecord::Schema.define(version: 2020_09_17_072627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_09_17_065505) do
     t.text "blog"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "image"
   end
 
 end
