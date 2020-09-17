f1223cc41c3b6b7d9525ebdef120fa4d29a3528a
fozia@iuj.ac.jp <fozia@iuj.ac.jp>
2020-09-17 09:42:53 +0000

    deleting comments and updating html for image

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'blogs#index'
-  resources :blogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :blogs do
+    collection do
+      blog :confirm
+      patch :confirm
+    end
+  end
 end
