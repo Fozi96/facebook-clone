7e1c3360927522a6fe14d1e72f2780fbc7840ced
fozia@iuj.ac.jp <fozia@iuj.ac.jp>
2020-09-21 21:07:21 +0000

    created the login form

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'sessions/new'
   root to: 'blogs#index'
   resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :destroy]
   resources :blogs do
     collection do
       post :confirm
