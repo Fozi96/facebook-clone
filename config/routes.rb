cf014024d39b994dd1c6ccca097c52eeeebf567b
fozia@iuj.ac.jp <fozia@iuj.ac.jp>
2020-09-21 20:59:45 +0000

    redirected to show action after using registration_to be fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root to: 'blogs#index'
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
   resources :blogs do
+    collection do
+      post :confirm
+     patch :confirm
+   end
   end
 end
