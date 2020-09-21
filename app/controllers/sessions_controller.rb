5c924d39399716280e39af571420089da9ccb507
fozia@iuj.ac.jp <fozia@iuj.ac.jp>
2020-09-21 21:24:04 +0000

    include helper module for session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,15 @@
 class SessionsController < ApplicationController
   def new
   end
-end
+
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # ログイン成功した場合
+      session[:user_id] = user.id
+      redirect_to user_path(user.id)
     else
-      # ログイン失敗した場合
+      flash[:danger] = 'I failed to login'
+      render :new
     end
   end
+end
