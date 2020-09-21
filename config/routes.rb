c3b76c7b662c319d1435ea91112f2a1cafebcb1c
fozia@iuj.ac.jp <fozia@iuj.ac.jp>
2020-09-21 18:03:11 +0000

    added bootstrap version 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   root 'blogs#index'
   resources :blogs do
-    collection do
-      blog :confirm
-      patch :confirm
-    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_17_072627) do
+ActiveRecord::Schema.define(version: 2020_09_17_100838) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 2020_09_17_072627) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "image"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
