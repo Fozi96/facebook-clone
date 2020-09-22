57677b904acad6a66f3fe6fce5f92cccf209869e
fozia@iuj.ac.jp <fozia@iuj.ac.jp>
2020-09-22 20:49:21 +0000

    uploaded error images

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root to: 'blogs#index'
   resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   resources :blogs do
     collection do
       post :confirm
